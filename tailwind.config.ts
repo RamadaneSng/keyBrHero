22aa5d021545d89065cc2ca8615f2eadea0818df
RamadaneSng <iscoramadane@gmail.com>
2024-03-05 23:58:45 +0000

    animate cursor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -235,14 +235,15 @@ const config = {
           to: { height: "0" },
         },
         "cursor-bip": {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
+          "0%": { opacity: 1 },
+          "50%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "cursor-bip": "cursor-bip 2s ease-out infinite",
+        "cursor-bip": "cursor-bip 2s infinite",
       },
     },
   },
